b4be6ca833ef103970bde5e4ecb4d1336a4e2fe6
MarcBrejner <marc@kaabrejner.dk>
2019-06-18 11:56:19 +0200

    Added server class

---
+++ app/src/main/java/com/example/battleshots/Server.java
@@ -0,0 +1,62 @@
+package com.example.battleshots;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class Server {
+    FirebaseFirestore database;
+
+    public Server(){
+        database = FirebaseFirestore.getInstance();
+    }
+
+
+    public void addShipToDatabase(Ship ship){
+        database.collection("Ships")
+                .add(ship)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "ShipSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding Ship", e);
+                    }
+                });
+    }
+
+
+    public void getShipsFromDatabase(){
+        database.collection("Ships")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting ships", task.getException());
+                        }
+                    }
+                });
+    }
+
+
+
+}
