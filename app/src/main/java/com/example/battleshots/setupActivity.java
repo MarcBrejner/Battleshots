b3b2fbf08f2af2da6da94c6e93323f1a12a4a59d
chris <christian.gernsoe@gmail.com>
2019-06-18 11:29:34 +0200

    Added multi dex fix

--- app/src/main/java/com/example/battleshots/setupActivity.java
+++ app/src/main/java/com/example/battleshots/setupActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class setupActivity extends AppCompatActivity {
 
     public static int[] playerShips;
@@ -31,10 +33,14 @@ public class setupActivity extends AppCompatActivity {
     }
 
     public void savePositions(View view) {
-
         for(int i = 0; i < 25; i++) {
-            int btnId = 2131230759;
-
+            int btnId = 2131230759 + i;
+            Button btn = (Button) findViewById(btnId);
+            if(btn.getText() == "clicked") {
+                playerShips[i] = btnId;
+            }
         }
+
+        System.out.println(Arrays.toString(playerShips));
     }
 }
