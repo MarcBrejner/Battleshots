e91fffac0d5dc96c4224ab401da645bbd4238f15
chris <christian.gernsoe@gmail.com>
2019-06-18 11:08:45 +0200

    Added setup map

--- app/src/main/java/com/example/battleshots/setupActivity.java
+++ app/src/main/java/com/example/battleshots/setupActivity.java
@@ -3,10 +3,12 @@ package com.example.battleshots;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class setupActivity extends AppCompatActivity {
 
+    public static int[] playerShips;
     private String TAG = "Setup-UserInterface";
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +17,24 @@ public class setupActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        Toast.makeText(getApplicationContext(), "button clicked", Toast.LENGTH_SHORT).show();
+        Button btn = (Button) findViewById(view.getId());
+
+        if (btn.getText() != "clicked") {
+            btn.setText("clicked");
+        } else {
+            btn.setText("");
+        }
+
+        String text = Integer.toString(btn.getId());
+
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
 
+    public void savePositions(View view) {
+
+        for(int i = 0; i < 25; i++) {
+            int btnId = 2131230759;
+
+        }
+    }
 }
