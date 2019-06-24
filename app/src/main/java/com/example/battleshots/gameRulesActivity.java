6954e8292ded9683dc77eb1c0d10f6a3df02d659
chris <christian.gernsoe@gmail.com>
2019-06-24 22:47:18 +0200

    Added back button to game rules and instructions to battle activity

--- app/src/main/java/com/example/battleshots/gameRulesActivity.java
+++ app/src/main/java/com/example/battleshots/gameRulesActivity.java
@@ -2,11 +2,16 @@ package com.example.battleshots;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class gameRulesActivity extends AppCompatActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.battle_shots_rules);
+    }
+
+    public void onClick(View view) {
+        super.finish();
     }
 }
